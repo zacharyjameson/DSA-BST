8dcb3ae070f2dbd41e6347d26dd36e468cc72998
Zack Jameson <zacharymjameson@gmail.com>
2021-05-27 11:43:37 -0400

    replacewith, findmin

--- index.js
+++ index.js
@@ -91,4 +91,40 @@ class BinarySearchTree {
           throw new Error('Key Error');
       }
   }
+
+  _replaceWith(node){
+      if(this.parent) {
+          if(this == this.parent.left){
+              this.parent.left = node;
+          }
+          else if(this == this.parent.right) {
+              this.parent.right = node;
+          }
+
+          if(node) {
+              node.parent = this.parent;
+          }
+      }
+      else {
+          if(node) {
+              this.key = node.key;
+              this.value = node.value;
+              this.left = node.left;
+              this.right = node.right;
+          }
+          else {
+              this.key = null;
+              this.value = null;
+              this.left = null;
+              this.right = null;
+          }
+      }
+  }
+
+  _findMin() {
+      if(!this.left){
+          return this;
+      }
+      return this.left._findMin();
+  }
 }
