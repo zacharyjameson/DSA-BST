615b86c15e6edf79979847cdcca5ab55fa4b816c
Zack Jameson <zacharymjameson@gmail.com>
2021-05-27 11:50:05 -0400

    drills

---
+++ bst-drills.js
@@ -0,0 +1,19 @@
+const BinarySearchTree = require("./index");
+
+let BST = new BinarySearchTree();
+
+BST.insert("E");
+BST.insert("A");
+BST.insert("S");
+BST.insert("Y");
+BST.insert("Q");
+BST.insert("U");
+BST.insert("E");
+BST.insert("S");
+BST.insert("T");
+BST.insert("I");
+BST.insert("O");
+BST.insert("N");
+
+
+console.log(BST)
